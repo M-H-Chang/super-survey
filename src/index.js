eddfedf230e8d4916e5b3bc5c57ff69e15222be4
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-12 11:15:04 -0700

    :beers: add files
    Co-authored-by: Jeremy Banka <hello@jeremybanka.com>
    co-authored-by: Min Chang <minchangmhc@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
+import { createFirestoreInstance } from 'redux-firestore'
+import "./index.css"
+import App from "./App"
+import firebase from "./firebase"
+
+// import reportWebVitals from "./reportWebVitals"
+
+const store = createStore(rootReducer)
+
+const rrfProps = {
+  firebase,
+  config: {
+    userProfile: `users`,
+  },
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <App />
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById(`root`)
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals()
